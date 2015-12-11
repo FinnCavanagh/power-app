1a67606db4f9e6085b77e7ca997859806ba83f60
FinnCavanagh <f.c.cavanagh@gmail.com>
2015-12-11 18:53:07 +0000

    setting up folders and api

---
+++ config/routes.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    router  = express.Router();
+
+//here pull in controllers
+// var usersController = require('../controllers/usersController');
+
+
+
+// here define routes e.g. :
+
+// router.route('/')
+//   .get(usersController.usersIndex)
+
+module.exports = router;
