27610295e1371c838ef03b02414c4fa6b6a3508a
FinnCavanagh <f.c.cavanagh@gmail.com>
2015-12-15 10:45:56 +0000

    authentication back up

--- models/user.js
+++ models/user.js
@@ -2,14 +2,16 @@ var mongoose = require("mongoose");
 var bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({ 
-  username: { type: String },
-  fullname: { type: String },
-  phone_number: { type: String },
-  image: { type: String },
-  twitter_handle: { type: String },
-  contacts: [{ type: mongoose.Schema.ObjectId, ref: 'Recipient' }],
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  local: {
+    username: { type: String },
+    fullname: { type: String },
+    phone_number: { type: String },
+    image: { type: String },
+    twitter_handle: { type: String },
+    contacts: [{ type: mongoose.Schema.ObjectId, ref: 'Recipient' }],
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+  }
 });
 
 userSchema.statics.encrypt = function(password) {
